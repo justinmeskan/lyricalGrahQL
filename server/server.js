c056a8494944a7e6cc6e60b5198c2754ad77a59a
justin meskan <justinmeskan@justins-mbp-2.attlocal.net>
2018-11-06 12:58:00 -0800

    mongobd hooked

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://justin:1ProbleProble1@ds153593.mlab.com:53593/lyricaldbgraphql';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
