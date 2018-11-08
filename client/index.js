3eaca95b2ae441715297babe770a4da2970c5863
justin meskan <justinmeskan@justins-mbp-2.attlocal.net>
2018-11-08 12:59:33 -0800

    added a gql query and created the ability to inject a varialble from my react component into my gql query

--- client/index.js
+++ client/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+import App from './components/app';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
   	<ApolloProvider client={client}>
-  		<div>Lyrical</div>
+  		<Router history={hashHistory}>
+  			<Route path="/" component={App}>
+  				<IndexRoute component={SongList} />
+  				<Route path="song/new" component={SongCreate} />
+  			</Route>
+  		</Router>
   	</ApolloProvider>
   );
 };
